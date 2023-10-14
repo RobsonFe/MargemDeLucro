62979d158403e9b26c9e32351429cb8002a4cbde
RobsonFe <robson_20ferreira@hotmail.com>
2023-10-14 17:02:04 -0300

    Atualiza

--- script.js
+++ script.js
@@ -2,16 +2,12 @@
 
 function calcularMargem() {
 
-    if(precoVenda || custoUnitario == null){
-        alert("Preencha os campos!")
-    }
-
     const nomeProduto = document.getElementById('nomeProduto').value;
     const precoVenda = parseFloat(document.getElementById('precoVenda').value);
     const custoUnitario = parseFloat(document.getElementById('custoUnitario').value);
     
-    const margemDeContribuicao = (precoVenda - custoUnitario).toFixed(2);
-    const margemPercentual = (((precoVenda - custoUnitario) / precoVenda) * 100).toFixed(2);
+    const margemDeContribuicao = (precoVenda - custoUnitario).toFixed(2).replace('.',',');
+    const margemPercentual = (((precoVenda - custoUnitario) / precoVenda) * 100).toFixed(2).replace('.',',');
     
     document.getElementById('resultadoNomeProduto').textContent = nomeProduto;
     document.getElementById('resultadoMargemContribuicao').textContent = margemDeContribuicao;
@@ -25,4 +21,3 @@ function limparCampo(){
     document.getElementById('resultadoMargemContribuicao').textContent = '';
     document.getElementById('resultadoMargemPercentual').textContent = '';
 }
-
